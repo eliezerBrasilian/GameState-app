c365c27b18b6299e300e120d7359269cb73f6cdf
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-09-09 00:14:17 -0300

    added flatlist like pinterest

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import {View, FlatList, ActivityIndicator} from 'react-native';
+import {View, Dimensions} from 'react-native';
 import {useContext, useEffect, useState} from 'react';
 import {AuthContext} from '../../contexts/AuthContext';
 import Header from './Header';
@@ -10,18 +10,25 @@ import firestore from '@react-native-firebase/firestore';
 import FlatListHeader from './FlatlistHeader';
 import {BannerAd, BannerAdSize, TestIds} from 'react-native-google-mobile-ads';
 import {strings} from '../../assets/strings';
-
+import MasonryList from '@react-native-seoul/masonry-list';
 const adUnitId = __DEV__ ? TestIds.BANNER : strings.banner_ad;
 function Home() {
   const {user, updateInfo, isPopUpVisible} = useContext(AuthContext);
   const [games, setGames] = useState([]);
   const [gamesEmpty, setGamesEmpty] = useState(false);
   const [gamesAmount, setGamesAmount] = useState(0);
-  const [gamesLoaded, setLoadingGames] = useState(true);
+  const [tablet, SetTablet] = useState(false);
+  const {width} = Dimensions.get('window');
+  const [numColumns, setNumColumns] = useState(2);
   useEffect(() => {
+    getOrientation();
     loadGames();
   }, [updateInfo]);
 
+  function getOrientation() {
+    console.log(width);
+    if (width >= 500) setNumColumns(3);
+  }
   async function loadGames() {
     setGames([]);
     try {
@@ -62,12 +69,12 @@ function Home() {
       {gamesEmpty ? (
         <NoGames />
       ) : (
-        <FlatList
+        <MasonryList
           data={games}
           keyExtractor={(item, index) => item.key.toString()}
           renderItem={({item}) => <Card data={item} />}
           onEndReachedThreshold={0.1}
-          numColumns={2}
+          numColumns={numColumns}
           ListHeaderComponent={<FlatListHeader amount={gamesAmount} />}
           contentContainerStyle={{padding: 10}}
           ListFooterComponent={
